1cfcb33b9e098744adb8f0f4ed9f74283d1c6b27
Andrea Pollastri <scrivi@andreapollastri.net>
2024-11-04 22:38:34 +0100

    Filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Database\Eloquent\Model::unguard();
+
+        \Illuminate\Validation\Rules\Password::defaults(function () {
+            return \Illuminate\Validation\Rules\Password::min(8)
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised();
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/pixel/{uuid}', function ($uuid) {
+    // TODO: Implement pixel tracking
+
+    return response()->file(public_path('pixel.png'));
 });
